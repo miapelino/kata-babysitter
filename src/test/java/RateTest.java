a2eccb43dfc6966e3cfd1b40de41fa9307331c4b
[mia pelino] <[maria.pelino@gmail.com]>
2019-01-10 02:31:39 -0500

    Add getStandardHourlyRate tests for Families B & C

--- src/test/java/RateTest.java
+++ src/test/java/RateTest.java
@@ -21,6 +21,24 @@ public class RateTest {
         assertThat(actual, is(15));
     }
 
+    @Test
+    public void getStandardHourlyRateUsingFamilyBReturns12() {
+        Family familyB = getTestFamilyB();
+
+        int actual = rate.getStandardHourlyRate(familyB);
+
+        assertThat(actual, is(12));
+    }
+
+    @Test
+    public void getStandardHourlyRateUsingFamilyCReturns21() {
+        Family familyC = getTestFamilyC();
+
+        int actual = rate.getStandardHourlyRate(familyC);
+
+        assertThat(actual, is(21));
+    }
+
     private Family getTestFamilyA() {
         Family familyA = new Family();
         familyA.setTotalKids(2);
@@ -30,6 +48,21 @@ public class RateTest {
         return familyA;
     }
 
+    private Family getTestFamilyB() {
+        Family familyB = new Family();
+        familyB.setTotalKids(1);
+        familyB.setTotalDogs(0);
+        familyB.setTotalCats(2);
 
+        return familyB;
+    }
 
+    private Family getTestFamilyC() {
+        Family familyC = new Family();
+        familyC.setTotalKids(2);
+        familyC.setTotalDogs(3);
+        familyC.setTotalCats(0);
+
+        return familyC;
+    }
 }
